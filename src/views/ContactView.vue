6c3c5cbd32878b96f5a4ef8c94942f54bf41c00d
initheo <141139697+initheo@users.noreply.github.com>
2025-05-08 18:32:07 +0700

    fix: replace className with class for consistent HTML attribute usage

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -26,7 +26,7 @@
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1440 120"
-          className="fill-white"
+          class="fill-white"
         >
           <path
             d="M0,64L80,69.3C160,75,320,85,480,80C640,75,800,53,960,48C1120,43,1280,53,1360,58.7L1440,64L1440,120L1360,120C1280,120,1120,120,960,120C800,120,640,120,480,120C320,120,160,120,80,120L0,120Z"
@@ -466,6 +466,6 @@
 </template>
 
 <script setup>
-import Header from '../components/Header.vue'
-import Footer from '../components/Footer.vue'
+import Footer from '../components/Footer.vue';
+import Header from '../components/Header.vue';
 </script>
