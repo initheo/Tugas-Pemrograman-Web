126053bddcac2f62dda539e773646cfcef9009e0
Faiznurullah <faizn103a@gmail.com>
2025-07-09 01:46:07 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://laundrease.tugas1.id',
         changeOrigin: true,
         secure: false,
         configure: (proxy, options) => {
@@ -25,7 +25,7 @@ export default defineConfig({
         }
       },
       '/sanctum': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://laundrease.tugas1.id',
         changeOrigin: true,
         secure: false,
         configure: (proxy, options) => {
